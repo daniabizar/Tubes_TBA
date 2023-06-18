0adbaa4e2d48afc080211bc9303dfda33c420f71
AbizarDani <daniabizar.44@gmail.com>
2023-06-18 23:00:33 +0700

    Tubes Commit

---
+++ bacup.pas
@@ -0,0 +1,13 @@
+alert("Lexical Analyzer Result: All Valid\nParsing Result: Valid\nYour code run succesfully!");
+
+alert("Lexical Analyzer Result: Valid\nParsing Result: Not Valid\nPlease check the grammar!");
+
+
+
+document.getElementById('output').innerHTML = 'Both Lexical Analyzer and Parser Result is Not Valid\nPlease check if there is any typo and check the grammar!';
+
+display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 50vh;
